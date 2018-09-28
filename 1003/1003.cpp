cc4074a769b1462b732d0dc7fecd1208db170e66
shagody <3251504950@qq.com>
2018-09-28 18:42:46 +0800

    1003

--- 1003/1003.cpp
+++ 1003/1003.cpp
@@ -1,17 +1,30 @@
 #include<iostream>
 #include<iomanip>
 using namespace std;
+static const int MAX = 500;
+static const int INFTY = (1 << 21);//????cant understand
+static const int WHITE = 0;
+static const int GREY = 1;
+static const int BLACK = 2;
+
+int n, m, c1, c2, M[MAX][MAX], rescue[MAX];
+
+void dijkstra() {
+	int minv;
+	int d[MAX], color[MAX];
+
+}
+
 int main() {
-	int n,m,c1,c2;
-	int road[3][m];
-	int teams[500] = { 0 };
 	cin >> n >> m >> c1 >> c2;
 	for (int i = 0; i < n; i++) {
-		cin >> teams[i];
+		cin >> rescue[i];
 	}
+	int x, y, z;
 	for (int i = 0; i < m; i++) {
-		cin>>
+		cin >> x >> y >> z;
+		M[x][y] = z;
 	}
-
+	dijkstra();
 	return 0;
 }
